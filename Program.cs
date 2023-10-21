a80c4440baea514db32edd409496b8e2d9660159
Danielalde2409 <carlosjara2409@gmail.com>
2023-10-21 06:40:33 -0500

    Creacion de clase main 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+
+class Program {
+    public static void Main{string[ Args]{
+        Console.WritenLine("Hola Mundo 2");
+    }
+    
+    }
+}
